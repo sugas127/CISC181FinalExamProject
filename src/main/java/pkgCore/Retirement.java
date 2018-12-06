ec41b3638f326c7781f44c4e3d03166e170a0d15
chadd <chadd@128.4.52.188>
2018-12-05 20:46:19 -0500

    Initial commit.

---
+++ src/main/java/pkgCore/Retirement.java
@@ -0,0 +1,86 @@
+package pkgCore;
+
+import org.apache.poi.ss.formula.functions.*;
+
+public class Retirement {
+
+	private int iYearsToWork;
+	private double dAnnualReturnWorking;
+	private int iYearsRetired;
+	private double dAnnualReturnRetired;
+	private double dRequiredIncome;
+	private double dMonthlySSI;
+	
+	public Retirement() {
+		
+	}
+	
+	public Retirement(int iYearsToWork, double dAnnualReturnWorking, int iYearsRetired, double dAnnualReturnRetired,
+			double dRequiredIncome, double dMonthlySSI) {
+		super();
+		this.iYearsToWork = iYearsToWork;
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+		this.iYearsRetired = iYearsRetired;
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+		this.dRequiredIncome = dRequiredIncome;
+		this.dMonthlySSI = dMonthlySSI;
+	}
+	
+	public int getiYearsToWork() {
+		return iYearsToWork;
+	}
+
+	public void setiYearsToWork(int iYearsToWork) {
+		this.iYearsToWork = iYearsToWork;
+	}
+
+	public double getdAnnualReturnWorking() {
+		return dAnnualReturnWorking;
+	}
+
+	public void setdAnnualReturnWorking(double dAnnualReturnWorking) {
+		this.dAnnualReturnWorking = dAnnualReturnWorking;
+	}
+
+	public int getiYearsRetired() {
+		return iYearsRetired;
+	}
+
+	public void setiYearsRetired(int iYearsRetired) {
+		this.iYearsRetired = iYearsRetired;
+	}
+
+	public double getdAnnualReturnRetired() {
+		return dAnnualReturnRetired;
+	}
+
+	public void setdAnnualReturnRetired(double dAnnualReturnRetired) {
+		this.dAnnualReturnRetired = dAnnualReturnRetired;
+	}
+
+	public double getdRequiredIncome() {
+		return dRequiredIncome;
+	}
+
+	public void setdRequiredIncome(double dRequiredIncome) {
+		this.dRequiredIncome = dRequiredIncome;
+	}
+
+	public double getdMonthlySSI() {
+		return dMonthlySSI;
+	}
+
+	public void setdMonthlySSI(double dMonthlySSI) {
+		this.dMonthlySSI = dMonthlySSI;
+	}
+
+	public double AmountToSave()
+	{
+		return FinanceLib.pmt(dAnnualReturnWorking / 12, iYearsToWork * 12, 0, -TotalAmountSaved(), false);
+	}
+
+	public double TotalAmountSaved()
+	{
+		return FinanceLib.pv(dAnnualReturnRetired / 12, iYearsRetired * 12, dRequiredIncome - dMonthlySSI, 0, false);
+	}
+}
---
+++ src/test/java/pkgEmpty/RetirementTest.java
@@ -0,0 +1,37 @@
+package pkgEmpty;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkgCore.Retirement;
+
+public class RetirementTest {
+
+	@Test
+	public void TestPMT() {
+		double dAnnualReturnWorking = 0.07;
+		int iYearsToWork = 40;
+		int iYearsRetired = 20; 
+		double dAnnualReturnRetired = 0.02;
+		double dRequiredIncome = 10000;
+		double dMonthlySSI = 2642;
+
+		double ExpectedPay = -554.13;
+		double ExpectedTotal = -1454485.55;
+
+
+		Retirement rtm = new Retirement(iYearsToWork, dAnnualReturnWorking, 
+				iYearsRetired, dAnnualReturnRetired, dRequiredIncome, dMonthlySSI);
+		
+		double PV = rtm.TotalAmountSaved();
+		double PMT = rtm.AmountToSave();
+		
+		assertEquals(ExpectedTotal, PV, 0.01);
+		assertEquals(ExpectedPay, PMT, 0.01);
+		
+		System.out.println(PV);
+		System.out.println(PMT);
+	}
+
+}
